b71652170b9aa884cc3e74b00b9dc514007092e7
Chris Choi <chutsu@gmail.com>
2014-11-26 11:08:45 -0800

    Disable posthook for now

--- restful/common/posthook.py
+++ restful/common/posthook.py
@@ -2,27 +2,27 @@
 from audi.cdap import ClientRestClient
 
 
-client = ClientRestClient()
-
-
-apps = [
-    'PurchaseHistory',
-    'HelloWorld',
-    'WordCount'
-]
-
-# stop flows
-for app_id in apps:
-    flows = client.flows(app_id).json()
-    for flow_id in flows:
-        client.stop_element(app_id, 'flows', flow_id)
-
-# stop procedures
-for app_id in apps:
-    procedures = client.procedures(app_id).json()
-    for procedure_id in procedures:
-        client.stop_element(app_id, 'procedures', procedure_id)
-
-# delete apps
-for app_id in apps:
-    client.delete_app(app_id)
+# client = ClientRestClient()
+#
+#
+# apps = [
+#     'PurchaseHistory',
+#     'HelloWorld',
+#     'WordCount'
+# ]
+#
+# # stop flows
+# for app_id in apps:
+#     flows = client.flows(app_id).json()
+#     for flow_id in flows:
+#         client.stop_element(app_id, 'flows', flow_id)
+#
+# # stop procedures
+# for app_id in apps:
+#     procedures = client.procedures(app_id).json()
+#     for procedure_id in procedures:
+#         client.stop_element(app_id, 'procedures', procedure_id)
+#
+# # delete apps
+# for app_id in apps:
+#     client.delete_app(app_id)
