03b806e3ad3d12a7487d607ed8e6fc262114673a
poorna <poorna@cask.co>
2015-12-01 18:39:29 -0800

    Add javadocs

--- long-running-test/src/main/java/co/cask/cdap/test/LongRunningTest.java
+++ long-running-test/src/main/java/co/cask/cdap/test/LongRunningTest.java
@@ -22,13 +22,44 @@ package co.cask.cdap.test;
  * @param <T> Type of test state
  */
 public interface LongRunningTest<T extends TestState> {
+  /**
+   * Called the first time this test is run against a fresh cluster.
+   * Can be used to deploy apps, start programs, etc.
+   */
   void setup() throws Exception;
 
+  /**
+   * Called after the last run of the test.
+   * Can be used to stop apps, and run other clean up operations.
+   * The cluster will be reset after this method.
+   * This method may never be called, instead cluster can simply be deleted.
+   */
   void cleanup() throws Exception;
 
+  /**
+   * Called when saved state for the test is not found (during the first run of the test)
+   *
+   * @return initial state object
+   */
   T getInitialState();
 
+  /**
+   * Called during every run of the test.
+   * Data operations performed in previous runs can be verified using the saved state.
+   * #runOperations will be called after this method.
+   * If this method throws exception then #runOperations will not be called, and test will fail.
+   *
+   * @param state state saved from previous call to #runOperations
+   */
   void verifyRuns(T state) throws Exception;
 
+  /**
+   * Called during every run of the test after #verifyRuns.
+   * All data operations for a test run are performed here.
+   * This method will not be called if #verifyRuns throws exception.
+   *
+   * @param state state saved from previous call to this method
+   * @return state of current run
+   */
   T runOperations(T state) throws Exception;
 }
