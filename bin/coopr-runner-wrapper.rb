42dbbad0554fe0b8d21c6508f3b4431f42f24e44
Derek Wood <derek@cask.co>
2015-12-09 15:52:02 -0800

    first cut of coopr-runner-wrapper and configs

---
+++ bin/coopr-runner-wrapper.rb
@@ -0,0 +1,237 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+#
+# Copyright © 2012-2015 Cask Data, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'json'
+require 'optparse'
+#require 'rest-client'
+#require 'time'
+#require 'open3'
+#require 'deep_merge/rails_compat'
+
+# Do not buffer output
+$stdout.sync = true
+
+module Cask
+  module CooprDriver
+    class ConfigWrapper
+
+      def initialize()
+        @original_argv = ARGV.dup
+        @conf_dir = _config_dir
+        @coopr_runner_exe = _coopr_runner_exe
+
+        _parse_opts
+      end
+
+      # Determine list of known dimensions (with corresponding configs) that apply
+      def identify_dimensions()
+        _dimensions = []
+        _dimensions.push('all')
+        _dimensions += _extract_dimensions_from_template_name()
+        _dimensions += _extract_dimensions_from_env()
+        _dimensions
+      end
+
+      # Generates the additional cmdline parameters from the identified dimensions
+      def generate_new_args()
+        @dimensions ||= identify_dimensions
+        _new_args = []
+        @dimensions.each do |dimension|
+          _new_arg = _generate_new_arg_for_dimension(dimension)
+          _new_args += _new_arg
+        end
+        _new_args
+      end
+
+      # Exec this process into coopr-runner.rb with the new generated config arguments
+      def run()
+        run_as_noop unless _action_create?
+        _validate_opts
+        @dimensions ||= identify_dimensions
+        @new_args ||= generate_new_args
+        _ruby_exec(@new_args + ARGV)
+      end
+
+      # Exec this process into coopr-runner.rb with the original ARGV. This can be called
+      # in any error condition to render this wrapper-script a no-op
+      def run_as_noop()
+        _ruby_exec(ARGV)
+      end
+
+      private
+
+      def _ruby_exec(argv)
+        ruby_bin = File.join(RbConfig::CONFIG['bindir'], RbConfig::CONFIG['ruby_install_name'])
+        exec_args = [ ruby_bin, @coopr_runner_exe ] + argv
+
+        # Exec into the new process
+        exec(*exec_args)
+      end
+
+      def _config_dir
+        if ! ENV['COOPR_RUNNER_WRAPPER_CONF_DIR'].nil?
+          ENV['COOPR_RUNNER_WRAPPER_CONF_DIR']
+        else
+          # Default to ../conf
+          File.join(File.dirname(File.expand_path(__FILE__)), '../conf')
+        end
+      end
+
+      def _coopr_runner_exe
+        if ! ENV['COOPR_RUNNER_WRAPPER_EXE'].nil?
+          ENV['COOPR_RUNNER_WRAPPER_EXE']
+        else
+          # Default to coopr-runner.rb in the same directory
+          File.join(File.dirname(File.expand_path(__FILE__)), 'coopr-runner.rb')
+        end
+      end
+
+      # Parse the options that we care about in the wrapper
+      def _parse_opts()
+        # Define the subset of coopr-runner.rb options that we care about here in the wrapper
+        op = OptionParser.new do |opts|
+          opts.on('-a', '--action ACTION', '"create", "reconfigure[-without-restart], "add-services", "[re]start", "stop", or "delete". Defaults to "create"') do |a|
+            @action = a
+          end
+          opts.on('-T', '--cluster-template CLUSTERTEMPLATE', 'ClusterTemplate, defaults to ENV[\'COOPR_DRIVER_CLUSTERTEMPLATE\'] else "cdap-singlenode-insecure-autobuild"') do |t|
+            @cluster_template = t
+          end
+          opts.on('-n', '--name NAME', 'Cluster Name, defaults to ENV[\'COOPR_DRIVER_NAME\'] else "coopr-driver"') do |n|
+            @name = n
+          end
+        end
+
+        # Extract only the options we care about from ARGV
+        relevant_opts = [ '-a', '--action', '-T', '--cluster-template', '-n', '--name' ]
+        relevant_argv = @original_argv.select do |element|
+          # select this element if it, or the previous element, is in relevant_opts (both key and value)
+          relevant_opts.include?(element) || relevant_opts.include?(@original_argv[@original_argv.find_index(element) - 1])
+        end
+        
+        # Parse this subset, otherwise we'd have to duplicate all options in coopr-runner.rb here in the wrapper
+        op.parse(relevant_argv)
+
+        # Clustertemplate and Name can be specified via ENV
+        @cluster_template ||= ENV['COOPR_DRIVER_CLUSTERTEMPLATE']
+        @name ||= ENV['COOPR_DRIVER_NAME']
+
+      rescue OptionParser::InvalidArgument, OptionParser::InvalidOption => e
+        puts "Invalid Argument/Options: #{$ERROR_INFO}"
+        exit 1
+      rescue => e
+        puts "ERROR: #{e.message}"
+        run_as_noop
+      end
+
+      def _validate_opts()
+        @cluster_template || fail('No clustertemplate given to wrapper')
+        @name || fail('No name argument given to wrapper')
+      end
+
+
+      # --action create, or no --action passed
+      def _action_create?()
+        @action.nil? || @action =~ /create/
+      end
+
+      def _extract_dimensions_from_template_name()
+ 
+        # Example names:
+        #   cdap-distributed-autobuild.json
+        #   cdap-distributed-insecure-autobuild.json
+        #   cdap-distributed-secure-hadoop-autobuild.json
+
+        _dimensions = []
+        _name = @cluster_template.downcase.split('-')
+
+        # First determine autobuild or not
+        if _name[-1] == 'autobuild'
+          _dimensions.push('autobuild')
+          _name.pop
+        end
+
+        # Check for base and singlenode/distributed
+        if _name[0] == 'cdap'
+          if _name[1] == 'mapr'
+            _dimensions.push('cdap_mapr')
+            _name.shift && _name.shift
+          elsif _name[1] == 'singlenode' || _name[1] == 'distributed'
+            _dimensions.push('cdap')
+            _name.shift
+          end
+        end
+
+        # Next should be singlenode/distributed
+        if _name[0] == 'singlenode'
+          _dimensions.push('singlenode')
+          _name.shift
+        elsif _name[0] == 'distributed'
+          _dimensions.push('distributed')
+          _name.shift
+        end
+
+        # check for insecure
+        if _name[0] != 'insecure'
+          _dimensions.push('auth')
+        end
+
+        # check for kerberos
+        if _name[0] == 'secure' && _name[1] == 'hadoop'
+          _dimensions.push('kerberos')
+        end
+        _dimensions
+      end
+
+      def _extract_dimensions_from_env()
+        _dimensions = []
+        unless ENV['bamboo_shortJobKey'].nil?
+          _dimensions.push(ENV['bamboo_shortJobKey'])
+        end
+        _dimensions
+      end
+
+      def _generate_new_arg_for_dimension(dimension)
+        res_args = []
+        input_file = File.join(@conf_dir, "#{dimension}.json")
+
+        begin
+          dimension_json = JSON.parse(IO.read(input_file))
+
+          # Top-level keys map to cmdline arg
+          dimension_json.each do |k, v|
+            arg_k = "--#{k}"
+            # value may either be json (in the case of --config) or just a string (--services)
+            if v.is_a?(String)
+              arg_v = v
+            else 
+              arg_v = JSON.generate(v)
+            end
+            res_args += [arg_k, arg_v]
+          end 
+        rescue => e
+          puts "WARNING: Unable to process input config file #{input_file}: #{e.message}"
+        end
+        res_args
+      end
+    end
+  end
+end
+
+ccc = Cask::CooprDriver::ConfigWrapper.new
+ccc.run
+
