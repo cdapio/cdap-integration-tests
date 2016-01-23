86dbbc4c4dfd44f4b140e7d690d5f99316d04d5e
shankar <shankar@cask.co>
2016-01-22 18:58:55 -0800

    schema validation tests

--- integration-test-remote/src/test/java/co/cask/cdap/app/etl/batch/ETLMapReduceTest.java
+++ integration-test-remote/src/test/java/co/cask/cdap/app/etl/batch/ETLMapReduceTest.java
@@ -46,8 +46,12 @@ import com.google.common.collect.Lists;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
@@ -113,12 +117,12 @@ public class ETLMapReduceTest extends ETLTestBase {
      *                                |------------ hdfs2 sink (1 row)
      */
 
-    ETLStage source = new ETLStage("SourceTable", new Plugin("Table",
-                                                       ImmutableMap.of(Properties.BatchReadableWritable.NAME, "input",
-                                                                       Properties.Table.PROPERTY_SCHEMA_ROW_FIELD,
-                                                                       "rowkey",
-                                                                       Properties.Table.PROPERTY_SCHEMA,
-                                                                       purchaseSchema.toString())));
+    ETLStage source = new ETLStage("SourceTable",
+                                   new Plugin("Table",
+                                              ImmutableMap.of(Properties.BatchReadableWritable.NAME, "input",
+                                                              Properties.Table.PROPERTY_SCHEMA_ROW_FIELD, "rowkey",
+                                                              Properties.Table.PROPERTY_SCHEMA,
+                                                              purchaseSchema.toString())));
     ETLStage lpFilter = new ETLStage("LowPassFilter", new Plugin(
       "ScriptFilter", ImmutableMap.of("script",
                                       "function shouldFilter(inputRecord) { return inputRecord.count > 8; }")));
@@ -150,8 +154,12 @@ public class ETLMapReduceTest extends ETLTestBase {
       new Connection("HighPassFilter", "TPFSAvro"),
       new Connection("LowPassFilter", "TPFSAvro2"));
 
-    ETLBatchConfig etlConfig = new ETLBatchConfig(ETLBatchConfig.Engine.MAPREDUCE,
-                                                  "* * * * *", source, sinks, transforms, connections, null, null);
+    ETLBatchConfig etlConfig = ETLBatchConfig.builder("* * * * *")
+      .setSource(source)
+      .addSinks(sinks)
+      .addTransforms(transforms)
+      .addConnections(connections)
+      .build();
 
     AppRequest<ETLBatchConfig> appRequest = getBatchAppRequest(etlConfig);
     Id.Application appId = Id.Application.from(Id.Namespace.DEFAULT, "TabToTab");
@@ -161,12 +169,6 @@ public class ETLMapReduceTest extends ETLTestBase {
     final MapReduceManager mrManager = appManager.getMapReduceManager(ETLMapReduce.NAME);
     mrManager.start();
     mrManager.waitForFinish(5, TimeUnit.MINUTES);
-    Tasks.waitFor(1, new Callable<Integer>() {
-      @Override
-      public Integer call() throws Exception {
-        return mrManager.getHistory(ProgramRunStatus.COMPLETED).size();
-      }
-    }, 1, TimeUnit.MINUTES);
 
     QueryClient client = new QueryClient(getClientConfig());
 
@@ -191,6 +193,287 @@ public class ETLMapReduceTest extends ETLTestBase {
     Assert.assertEquals(1, resultList.size());
     verifyResult("row1", resultList.get(0).getColumns());
   }
+
+  @Test(expected =  IOException.class)
+  public void testDAGInvalidSchema() throws Exception {
+
+    /*                                  |----- userRewardsSink (TPFS)
+     *                                  |
+     *                                  |
+     *             -----  userRewards <rowkey, userid, rewards> --| projection <rowKey, rewards> ----
+     *             |                                                                                  |---rewardsSink
+     * sourceTable ------ itemRewards <rowkey, item, rewards> ----------------(invalid)---------------
+     * (2 rows)                       |
+     *                                |------------ itemRewardsSink (TPFS)
+     */
+
+    ETLStage source = new ETLStage("SourceTable",
+                                   new Plugin("Table",
+                                              ImmutableMap.of(Properties.BatchReadableWritable.NAME, "input",
+                                                              Properties.Table.PROPERTY_SCHEMA_ROW_FIELD,
+                                                              "rowkey",
+                                                              Properties.Table.PROPERTY_SCHEMA,
+                                                              purchaseSchema.toString())));
+    Schema userRewardsSchema = Schema.recordOf(
+      "rewards",
+      Schema.Field.of("rowkey", Schema.of(Schema.Type.STRING)),
+      Schema.Field.of("userId", Schema.of(Schema.Type.STRING)),
+      Schema.Field.of("rewards", Schema.of(Schema.Type.INT))
+    );
+    Schema itemRewardsSchema = Schema.recordOf(
+      "rewards",
+      Schema.Field.of("rowkey", Schema.of(Schema.Type.STRING)),
+      Schema.Field.of("item", Schema.of(Schema.Type.STRING)),
+      Schema.Field.of("rewards", Schema.of(Schema.Type.INT))
+    );
+    ETLStage userRewards = new ETLStage("userRewardsTransform", new Plugin(
+      "JavaScript", ImmutableMap.of("script",
+                                    "function transform(input, emitter, context) " +
+                                      "{ emitter.emit({" +
+                                      "'rowkey' : input.rowkey," +
+                                      "'userId' : input.userId," +
+                                      "'rewards' : 10" +
+                                      "}); }",
+                                    "schema", userRewardsSchema.toString()
+    )));
+    ETLStage itemRewards = new ETLStage("itemRewardsTransform", new Plugin(
+      "JavaScript", ImmutableMap.of("script",
+                                    "function transform(input, emitter, context) " +
+                                      "{ emitter.emit({" +
+                                      "'rowkey' : input.rowkey," +
+                                      "'item' : input.item," +
+                                      "'rewards' : 5" +
+                                      "}); }",
+                                    "schema", itemRewardsSchema.toString()
+    )));
+
+    ETLStage userDropProjection = new ETLStage("userProjection", new Plugin("Projection",
+                                                                            ImmutableMap.of("drop", "userId")));
+    List<ETLStage> transforms = ImmutableList.of(userRewards, itemRewards, userDropProjection);
+    Schema rewardsSchema = Schema.recordOf(
+      "rewards",
+      Schema.Field.of("rowkey", Schema.of(Schema.Type.STRING)),
+      Schema.Field.of("rewards", Schema.of(Schema.Type.INT))
+    );
+
+    ETLStage allRewardsSink = new ETLStage("allRewards", new Plugin(
+      "TPFSAvro", ImmutableMap.of(Properties.TimePartitionedFileSetDataset.SCHEMA, rewardsSchema.toString(),
+                                  Properties.TimePartitionedFileSetDataset.TPFS_NAME, "allRewards")));
+
+    ETLStage userRewardsSink = new ETLStage("userRewards", new Plugin(
+      "TPFSAvro", ImmutableMap.of(Properties.TimePartitionedFileSetDataset.SCHEMA, userRewardsSchema.toString(),
+                                  Properties.TimePartitionedFileSetDataset.TPFS_NAME, "userRewards")));
+
+    ETLStage itemRewardsSink = new ETLStage("itemRewards", new Plugin(
+      "TPFSAvro", ImmutableMap.of(Properties.TimePartitionedFileSetDataset.SCHEMA, itemRewardsSchema.toString(),
+                                  Properties.TimePartitionedFileSetDataset.TPFS_NAME, "itemRewards")));
+
+    List<ETLStage> sinks = ImmutableList.of(allRewardsSink, userRewardsSink, itemRewardsSink);
+
+    List<Connection> connections = ImmutableList.of(
+      new Connection("SourceTable", "userRewardsTransform"),
+      new Connection("SourceTable", "itemRewardsTransform"),
+      new Connection("userRewardsTransform", "userProjection"),
+      new Connection("userProjection", "allRewards"),
+      new Connection("itemRewardsTransform", "allRewards"),
+      new Connection("userRewardsTransform", "userRewards"),
+      new Connection("itemRewardsTransform", "itemRewards")
+    );
+
+    ETLBatchConfig etlConfig = ETLBatchConfig.builder("* * * * *")
+      .setSource(source)
+      .addSinks(sinks)
+      .addTransforms(transforms)
+      .addConnections(connections)
+      .build();
+
+    AppRequest<ETLBatchConfig> appRequest = getBatchAppRequest(etlConfig);
+    Id.Application appId = Id.Application.from(Id.Namespace.DEFAULT, "TabToTab");
+    // deploy would fail
+    deployApplication(appId, appRequest);
+  }
+
+  @Test
+  public void testDAGSchemaChanges() throws Exception {
+
+    /*                                  |----- userRewardsSink (TPFS)(2 rows)
+     *                                  |
+     *                                  |
+     *             -----  userRewards <rowkey, userid, rewards> --| projection <rowKey, rewards> ----
+     *             |                                                                                  |---rewardsSink
+     * sourceTable ------ itemRewards <rowkey, item, rewards> -----| projection <rowKey, rewards> ---      TPFS(4 rows)
+     * (2 rows)                       |
+     *                                |------------ itemRewardsSink (TPFS) (2 row)
+     */
+
+    ETLStage source = new ETLStage("SourceTable",
+                                   new Plugin("Table",
+                                              ImmutableMap.of(Properties.BatchReadableWritable.NAME, "input",
+                                                              Properties.Table.PROPERTY_SCHEMA_ROW_FIELD,
+                                                              "rowkey",
+                                                              Properties.Table.PROPERTY_SCHEMA,
+                                                              purchaseSchema.toString())));
+    Schema userRewardsSchema = Schema.recordOf(
+      "rewards",
+      Schema.Field.of("rowkey", Schema.of(Schema.Type.STRING)),
+      Schema.Field.of("userId", Schema.of(Schema.Type.STRING)),
+      Schema.Field.of("rewards", Schema.of(Schema.Type.INT))
+    );
+    Schema itemRewardsSchema = Schema.recordOf(
+      "rewards",
+      Schema.Field.of("rowkey", Schema.of(Schema.Type.STRING)),
+      Schema.Field.of("item", Schema.of(Schema.Type.STRING)),
+      Schema.Field.of("rewards", Schema.of(Schema.Type.INT))
+    );
+    ETLStage userRewards = new ETLStage("userRewardsTransform", new Plugin(
+      "JavaScript", ImmutableMap.of("script",
+                                    "function transform(input, emitter, context) " +
+                                      "{ emitter.emit({" +
+                                      "'rowkey' : input.rowkey," +
+                                      "'userId' : input.userId," +
+                                      "'rewards' : 10" +
+                                      "}); }",
+                                    "schema", userRewardsSchema.toString()
+    )));
+
+    ETLStage itemRewards = new ETLStage("itemRewardsTransform", new Plugin(
+      "JavaScript", ImmutableMap.of("script",
+                                    "function transform(input, emitter, context) " +
+                                      "{ emitter.emit({" +
+                                      "'rowkey' : input.rowkey," +
+                                      "'item' : input.item," +
+                                      "'rewards' : 5" +
+                                      "}); }",
+                                    "schema", itemRewardsSchema.toString()
+    )));
+    ETLStage userDropProjection = new ETLStage("userProjection", new Plugin("Projection",
+                                                                            ImmutableMap.of("drop", "userId")));
+    ETLStage itemDropProjection = new ETLStage("itemProjection", new Plugin("Projection",
+                                                                            ImmutableMap.of("drop", "item")));
+    List<ETLStage> transforms = ImmutableList.of(userRewards, itemRewards, userDropProjection, itemDropProjection);
+
+    Schema rewardsSchema = Schema.recordOf(
+      "rewards",
+      Schema.Field.of("rowkey", Schema.of(Schema.Type.STRING)),
+      Schema.Field.of("rewards", Schema.of(Schema.Type.INT))
+    );
+
+    ETLStage allRewardsSink = new ETLStage("allRewards", new Plugin(
+      "TPFSAvro", ImmutableMap.of(Properties.TimePartitionedFileSetDataset.SCHEMA, rewardsSchema.toString(),
+                                  Properties.TimePartitionedFileSetDataset.TPFS_NAME, "allRewards")));
+
+    ETLStage userRewardsSink = new ETLStage("userRewards", new Plugin(
+      "TPFSAvro", ImmutableMap.of(Properties.TimePartitionedFileSetDataset.SCHEMA, userRewardsSchema.toString(),
+                                  Properties.TimePartitionedFileSetDataset.TPFS_NAME, "userRewards")));
+
+    ETLStage itemRewardsSink = new ETLStage("itemRewards", new Plugin(
+      "TPFSAvro", ImmutableMap.of(Properties.TimePartitionedFileSetDataset.SCHEMA, itemRewardsSchema.toString(),
+                                  Properties.TimePartitionedFileSetDataset.TPFS_NAME, "itemRewards")));
+
+    List<ETLStage> sinks = ImmutableList.of(allRewardsSink, userRewardsSink, itemRewardsSink);
+
+    List<Connection> connections = ImmutableList.of(
+      new Connection("SourceTable", "userRewardsTransform"),
+      new Connection("SourceTable", "itemRewardsTransform"),
+      new Connection("userRewardsTransform", "userProjection"),
+      new Connection("itemRewardsTransform", "itemProjection"),
+      new Connection("userProjection", "allRewards"),
+      new Connection("itemProjection", "allRewards"),
+      new Connection("userRewardsTransform", "userRewards"),
+      new Connection("itemRewardsTransform", "itemRewards")
+    );
+
+    ETLBatchConfig etlConfig = ETLBatchConfig.builder("* * * * *")
+      .setSource(source)
+      .addSinks(sinks)
+      .addTransforms(transforms)
+      .addConnections(connections)
+      .build();
+
+    AppRequest<ETLBatchConfig> appRequest = getBatchAppRequest(etlConfig);
+    Id.Application appId = Id.Application.from(Id.Namespace.DEFAULT, "TabToTab");
+    ApplicationManager appManager = deployApplication(appId, appRequest);
+    ingestPurchaseTestData(getTableDataset("input"));
+
+    final MapReduceManager mrManager = appManager.getMapReduceManager(ETLMapReduce.NAME);
+    mrManager.start();
+    mrManager.waitForFinish(5, TimeUnit.MINUTES);
+
+    QueryClient client = new QueryClient(getClientConfig());
+    ExploreExecutionResult result = client.execute(Id.Namespace.DEFAULT, "select * from dataset_allRewards")
+      .get(5, TimeUnit.MINUTES);
+    Assert.assertEquals(QueryStatus.OpStatus.FINISHED, result.getStatus().getStatus());
+    List<QueryResult> resultList = Lists.newArrayList(result);
+    Assert.assertEquals(4, resultList.size());
+
+    Set<Rewards> rewards = new HashSet();
+    rewards.add(new Rewards("row1", 5));
+    rewards.add(new Rewards("row1", 10));
+    rewards.add(new Rewards("row2", 5));
+    rewards.add(new Rewards("row2", 10));
+    verifyRewardResults(resultList, rewards, "rewards");
+
+    result = client.execute(Id.Namespace.DEFAULT, "select * from dataset_userRewards")
+      .get(5, TimeUnit.MINUTES);
+    Assert.assertEquals(QueryStatus.OpStatus.FINISHED, result.getStatus().getStatus());
+    resultList = Lists.newArrayList(result);
+    Assert.assertEquals(2, resultList.size());
+    rewards = new HashSet();
+    rewards.add(new Rewards("row1", null, "samuel", 10));
+    rewards.add(new Rewards("row2", null, "jackson", 10));
+    verifyRewardResults(resultList, rewards, "user");
+
+    result = client.execute(Id.Namespace.DEFAULT, "select * from dataset_itemRewards")
+      .get(5, TimeUnit.MINUTES);
+    Assert.assertEquals(QueryStatus.OpStatus.FINISHED, result.getStatus().getStatus());
+    resultList = Lists.newArrayList(result);
+    Assert.assertEquals(2, resultList.size());
+    rewards = new HashSet();
+    rewards.add(new Rewards("row1", "scotch", null, 5));
+    rewards.add(new Rewards("row2", "island", null, 5));
+    verifyRewardResults(resultList, rewards, "item");
+  }
+
+  // internal test class used for verifying results, used by testDAGSchemaChanges.
+  private class Rewards {
+    private String rowKey;
+    private String userId;
+    private String item;
+    private int rewards;
+
+    private Rewards(String rowKey, String item, String userId, int rewards) {
+      this.rowKey = rowKey;
+      this.rewards = rewards;
+      this.userId = userId;
+      this.item = item;
+    }
+
+    private Rewards(String rowKey, int rewards) {
+      this(rowKey, null, null, rewards);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      } else if (o != null && this.getClass() == o.getClass()) {
+        Rewards that = (Rewards) o;
+        return Objects.equals(this.rowKey, that.rowKey) && Objects.equals(this.rewards, that.rewards) &&
+          Objects.equals(this.item, that.item) && Objects.equals(this.userId, that.userId);
+      } else {
+        return false;
+      }
+    }
+
+    public int hashCode() {
+      return Objects.hash(new Object[]{this.rowKey, this.userId, this.item, this.rewards});
+    }
+
+    public String toString() {
+      return String.format("Rowkey : %s, userId : %s, item : %s, rewards : %s ", rowKey, userId, item, rewards);
+    }
+  }
+
 
   @SuppressWarnings("ConstantConditions")
   @Test
@@ -263,6 +546,33 @@ public class ETLMapReduceTest extends ETLTestBase {
 
     row = outputTable.get(Bytes.toBytes("row2"));
     Assert.assertEquals(0, row.getColumns().size());
+  }
+
+  private void verifyRewardResults(List<QueryResult> queryResults, Set<Rewards> expected, String type) {
+    Set<Rewards> actual = new HashSet<>();
+    for (QueryResult queryResult : queryResults) {
+      Rewards rewards = null;
+      switch (type) {
+        case "rewards":
+          rewards = new Rewards((String) queryResult.getColumns().get(0),
+                                (Integer) (queryResult.getColumns().get(1)));
+          break;
+        case "user":
+          rewards = new Rewards((String) queryResult.getColumns().get(0), null,
+                                (String) (queryResult.getColumns().get(1)),
+                                (Integer) (queryResult.getColumns().get(2)));
+          break;
+        case "item":
+          rewards = new Rewards((String) queryResult.getColumns().get(0), (String) (queryResult.getColumns().get(1)),
+                                null,
+                                (Integer) (queryResult.getColumns().get(2)));
+          break;
+        default:
+          Assert.fail(String.format("Unrecognized type {%s}!", type));
+      }
+      actual.add(rewards);
+    }
+    Assert.assertEquals(expected, actual);
   }
 
   private void verifyResult(String rowKey, List<Object> row) {
