c7f0c8643d856ecfc05c1db4a7ac7586bf1b1842
romy-khetan <romy_khetan@NGD11115.persistent.co.in>
2016-07-15 20:39:13 +0530

    Removed unused import

--- integration-test-remote/src/test/java/co/cask/cdap/app/etl/batch/ExcelInputReaderTest.java
+++ integration-test-remote/src/test/java/co/cask/cdap/app/etl/batch/ExcelInputReaderTest.java
@@ -31,7 +31,6 @@ import co.cask.cdap.etl.common.Plugin;
 import co.cask.cdap.etl.proto.v2.ETLBatchConfig;
 import co.cask.cdap.etl.proto.v2.ETLPlugin;
 import co.cask.cdap.etl.proto.v2.ETLStage;
-import co.cask.cdap.proto.Id;
 import co.cask.cdap.proto.artifact.AppRequest;
 import co.cask.cdap.proto.id.ApplicationId;
 import co.cask.cdap.proto.id.NamespaceId;
