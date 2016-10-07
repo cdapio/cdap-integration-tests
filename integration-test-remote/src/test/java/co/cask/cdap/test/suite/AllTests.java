55e7f7d47a2f28dd0681be5bcf798e5d331c3d6e
CuriousVini <vinisha@cask.co>
2016-10-06 17:17:46 -0700

    Enable BatchJoinerTest

--- integration-test-remote/src/test/java/co/cask/cdap/test/suite/AllTests.java
+++ integration-test-remote/src/test/java/co/cask/cdap/test/suite/AllTests.java
@@ -20,6 +20,7 @@ import co.cask.cdap.app.etl.KVTableWithProjectionTest;
 import co.cask.cdap.app.etl.StreamTPFSWithProjectionTest;
 import co.cask.cdap.app.etl.batch.BatchAggregatorTest;
 import co.cask.cdap.app.etl.batch.BatchCubeSinkTest;
+import co.cask.cdap.app.etl.batch.BatchJoinerTest;
 import co.cask.cdap.app.etl.batch.CopybookReaderTest;
 import co.cask.cdap.app.etl.batch.ETLMapReduceTest;
 import co.cask.cdap.app.etl.batch.ExcelInputReaderTest;
@@ -65,6 +66,7 @@ import org.junit.runners.Suite;
   ApplicationTest.class,
   BatchAggregatorTest.class,
   BatchCubeSinkTest.class,
+  BatchJoinerTest.class,
   CopybookReaderTest.class,
   DatasetTest.class,
   ETLMapReduceTest.class,
