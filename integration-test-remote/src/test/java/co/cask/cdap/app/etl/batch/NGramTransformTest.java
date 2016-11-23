21f3a6c43ebc39a8f99aa0b97c4bc6763128a29d
Shashank Moghe <shashank_moghe@persistent.com>
2016-11-23 14:35:46 +0530

    [HYDRATOR-1007] Added tokenization unit as a string parameter and changes for input for the plugin to be as string instead of array.

--- integration-test-remote/src/test/java/co/cask/cdap/app/etl/batch/NGramTransformTest.java
+++ integration-test-remote/src/test/java/co/cask/cdap/app/etl/batch/NGramTransformTest.java
@@ -40,9 +40,9 @@ import co.cask.cdap.test.suite.category.HDP21Incompatible;
 import co.cask.cdap.test.suite.category.HDP22Incompatible;
 import co.cask.cdap.test.suite.category.HDP23Incompatible;
 import co.cask.cdap.test.suite.category.MapR5Incompatible;
+import co.cask.hydrator.plugin.transform.ProjectionTransform;
 import com.google.common.collect.ImmutableMap;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
@@ -73,106 +73,103 @@ import java.util.concurrent.TimeUnit;
   // Currently, coopr doesn't provision MapR cluster with Spark. Enable this test once COOK-108 is fixed
   MapR5Incompatible.class // MapR5x category is used for all MapR version
 })
-// Refactor the test once https://github.com/caskdata/hydrator-plugins/pull/504 is merged.
-@Ignore
 public class NGramTransformTest extends ETLTestBase {
-    private static final Schema OUTPUT_SCHEMA = Schema.recordOf("outputSchema",
-            Schema.Field.of("ngrams", Schema.arrayOf(Schema.of(Schema.Type.STRING))));
+  private static final Schema OUTPUT_SCHEMA = Schema.recordOf("outputSchema",
+                                              Schema.Field.of("body", Schema.of(Schema.Type.STRING)),
+                                              Schema.Field.of("ngrams", Schema.arrayOf(Schema.of(Schema.Type.STRING))));
 
-    @Test
-    public void testNGramTransformWithTwoTerms() throws Exception {
-        String text = "textToNGramTwo";
-        ApplicationManager appManager = deployApplication(text, OUTPUT_SCHEMA, "/", "2");
-        StreamManager streamManager =
-                getTestManager().getStreamManager(Id.Stream.from(TEST_NAMESPACE, text));
-        streamManager.send("hi/hello/hie");
-        streamManager.send("how/are/you");
-        WorkflowManager workflowManager = appManager.getWorkflowManager(SmartWorkflow.NAME);
-        workflowManager.start();
-        workflowManager.waitForFinish(10, TimeUnit.MINUTES);
-        Assert.assertEquals(getExpectedForTwoTerms(), getNgramTerms());
+  @Test
+  public void testNGramTransformWithTwoTerms() throws Exception {
+    String text = "textToNGramTwo";
+    ApplicationManager appManager = deployApplication(text, OUTPUT_SCHEMA, "2");
+    StreamManager streamManager =
+      getTestManager().getStreamManager(Id.Stream.from(TEST_NAMESPACE, text));
+    streamManager.send("hi hello hie");
+    streamManager.send("how are you");
+    WorkflowManager workflowManager = appManager.getWorkflowManager(SmartWorkflow.NAME);
+    workflowManager.start();
+    workflowManager.waitForFinish(10, TimeUnit.MINUTES);
+    Assert.assertEquals(getExpectedForTwoTerms(), getNgramTerms());
+  }
+
+  @Test
+  public void testNGramTransformWithThreeTerms() throws Exception {
+    String text = "textToNGramThree";
+    ApplicationManager appManager = deployApplication(text, OUTPUT_SCHEMA, "3");
+    StreamManager streamManager =
+      getTestManager().getStreamManager(Id.Stream.from(TEST_NAMESPACE, text));
+    streamManager.send("hi hello hie hi");
+    WorkflowManager workflowManager = appManager.getWorkflowManager(SmartWorkflow.NAME);
+    workflowManager.start();
+    workflowManager.waitForFinish(10, TimeUnit.MINUTES);
+    Assert.assertEquals(getExpectedForThreeTerms(), getNgramTerms());
+  }
+
+  @Test(expected = NullPointerException.class)
+  //ngramSize is mandatory and if its null then NullPointerException is expected.
+  public void testNullNGramSize() throws Exception {
+    deployApplication("NegativeTestNGramSize", OUTPUT_SCHEMA, null);
+  }
+
+  private ApplicationManager deployApplication(String textsForNgram, Schema outputSchema,
+                                               String terms) throws Exception {
+    Map<String, String> sourceProperties = new ImmutableMap.Builder<String, String>()
+      .put("name", textsForNgram)
+      .put("duration", "1d")
+      .put("format", "text")
+      .build();
+    Map<String, String> ngramProperties = new ImmutableMap.Builder<String, String>()
+      .put("outputField", "ngrams")
+      .put("fieldToBeTransformed", "body")
+      .put("ngramSize", terms)
+      .put("tokenizationUnit", "word")
+      .build();
+    Map<String, String> sinkProperties = new ImmutableMap.Builder<String, String>()
+      .put("name", "ngramTable")
+      .put("schema", outputSchema.toString())
+      .build();
+    ETLBatchConfig etlConfig = ETLBatchConfig.builder("* * * * *")
+      .addStage(new ETLStage("source", new ETLPlugin("Stream",
+                                                     BatchSource.PLUGIN_TYPE, sourceProperties, null)))
+      .addStage(new ETLStage("userProjection",
+                             new ETLPlugin("Projection", ProjectionTransform.PLUGIN_TYPE,
+                                           ImmutableMap.of("drop", "ts,headers"),
+                                           null)))
+      .addStage(new ETLStage("ngramSpark", new ETLPlugin("NGramTransform",
+                                                         SparkCompute.PLUGIN_TYPE, ngramProperties, null)))
+      .addStage(new ETLStage("sink", new ETLPlugin("TPFSAvro",
+                                                   BatchSink.PLUGIN_TYPE, sinkProperties, null)))
+      .addConnection("source", "userProjection")
+      .addConnection("userProjection", "ngramSpark")
+      .addConnection("ngramSpark", "sink")
+      .build();
+    return deployApplication(Id.Application.from(TEST_NAMESPACE, "NGramTransform"),
+                             getBatchAppRequestV2(etlConfig));
+  }
+
+  private Set<String> getExpectedForTwoTerms() {
+    Set<String> expectedSet = new HashSet();
+    expectedSet.add("[\"hi hello\",\"hello hie\"]");
+    expectedSet.add("[\"how are\",\"are you\"]");
+    return expectedSet;
+  }
+
+  private Set<String> getExpectedForThreeTerms() {
+    Set<String> expectedSet = new HashSet();
+    expectedSet.add("[\"hi hello hie\",\"hello hie hi\"]");
+    return expectedSet;
+  }
+
+  private Set<String> getNgramTerms() throws ExecutionException, InterruptedException {
+    QueryClient queryClient = new QueryClient(getClientConfig());
+    ExploreExecutionResult exploreExecutionResult =
+      queryClient.execute(TEST_NAMESPACE, "SELECT * FROM dataset_ngramTable").get();
+
+    Set<String> classifiedMessages = new HashSet();
+    while (exploreExecutionResult.hasNext()) {
+      List<Object> columns = exploreExecutionResult.next().getColumns();
+      classifiedMessages.add((String) columns.get(1));
     }
-
-    @Test
-    public void testNGramTransformWithThreeTerms() throws Exception {
-        String text = "textToNGramThree";
-        ApplicationManager appManager = deployApplication(text, OUTPUT_SCHEMA, ",", "3");
-        StreamManager streamManager =
-                getTestManager().getStreamManager(Id.Stream.from(TEST_NAMESPACE, text));
-        streamManager.send("hi,hello,hie,hi");
-        WorkflowManager workflowManager = appManager.getWorkflowManager(SmartWorkflow.NAME);
-        workflowManager.start();
-        workflowManager.waitForFinish(10, TimeUnit.MINUTES);
-        Assert.assertEquals(getExpectedForThreeTerms(), getNgramTerms());
-    }
-
-    @Test(expected = NullPointerException.class)
-    //ngramSize is mandatory and if its null then NullPointerException is expected.
-    public void testNullNGramSize() throws Exception {
-        deployApplication("NegativeTestNGramSize", OUTPUT_SCHEMA, ",", null);
-    }
-
-    private ApplicationManager deployApplication(String textsForNgram, Schema outputSchema,
-                                                 String delimiter, String terms) throws Exception {
-        Map<String, String> sourceProperties = new ImmutableMap.Builder<String, String>()
-                .put("name", textsForNgram)
-                .put("duration", "1d")
-                .put("format", "text")
-                .build();
-        Map<String, String> tokenizerProperties = new ImmutableMap.Builder<String, String>()
-                .put("outputColumn", "words")
-                .put("columnToBeTokenized", "body")
-                .put("pattern", delimiter)
-                .build();
-        Map<String, String> ngramProperties = new ImmutableMap.Builder<String, String>()
-                .put("outputField", "ngrams")
-                .put("fieldToBeTransformed", "words")
-                .put("ngramSize", terms)
-                .build();
-        Map<String, String> sinkProperties = new ImmutableMap.Builder<String, String>()
-                .put("name", "ngramTable")
-                .put("schema", outputSchema.toString())
-                .build();
-        ETLBatchConfig etlConfig = ETLBatchConfig.builder("* * * * *")
-                .addStage(new ETLStage("source", new ETLPlugin("Stream",
-                        BatchSource.PLUGIN_TYPE, sourceProperties, null)))
-                .addStage(new ETLStage("tokenizerSpark", new ETLPlugin("Tokenizer",
-                        SparkCompute.PLUGIN_TYPE, tokenizerProperties, null)))
-                .addStage(new ETLStage("ngramSpark", new ETLPlugin("NGramTransform",
-                        SparkCompute.PLUGIN_TYPE, ngramProperties, null)))
-                .addStage(new ETLStage("sink", new ETLPlugin("TPFSAvro",
-                        BatchSink.PLUGIN_TYPE, sinkProperties, null)))
-                .addConnection("source", "tokenizerSpark")
-                .addConnection("tokenizerSpark", "ngramSpark")
-                .addConnection("ngramSpark", "sink")
-                .build();
-        return deployApplication(Id.Application.from(TEST_NAMESPACE, "NGramTransform"),
-                getBatchAppRequestV2(etlConfig));
-    }
-
-    private Set<String> getExpectedForTwoTerms() {
-        Set<String> expectedSet = new HashSet();
-        expectedSet.add("[\"hi hello\",\"hello hie\"]");
-        expectedSet.add("[\"how are\",\"are you\"]");
-        return expectedSet;
-    }
-
-    private Set<String> getExpectedForThreeTerms() {
-        Set<String> expectedSet = new HashSet();
-        expectedSet.add("[\"hi hello hie\",\"hello hie hi\"]");
-        return expectedSet;
-    }
-
-    private Set<String> getNgramTerms() throws ExecutionException, InterruptedException {
-        QueryClient queryClient = new QueryClient(getClientConfig());
-        ExploreExecutionResult exploreExecutionResult =
-                queryClient.execute(TEST_NAMESPACE, "SELECT * FROM dataset_ngramTable").get();
-
-        Set<String> classifiedMessages = new HashSet();
-        while (exploreExecutionResult.hasNext()) {
-            List<Object> columns = exploreExecutionResult.next().getColumns();
-            classifiedMessages.add((String) columns.get(0));
-        }
-        return classifiedMessages;
-    }
+    return classifiedMessages;
+  }
 }
