9605c5f10e74b8270df4c7a39baaa0e00383e1a7
poorna <poorna@cask.co>
2017-02-17 18:35:39 -0800

    Verify invalid data removal

--- long-running-test/src/test/java/co/cask/cdap/longrunning/txprune/InvalidListPruneTest.java
+++ long-running-test/src/test/java/co/cask/cdap/longrunning/txprune/InvalidListPruneTest.java
@@ -22,7 +22,10 @@ import co.cask.cdap.client.util.RESTClient;
 import co.cask.cdap.common.StreamNotFoundException;
 import co.cask.cdap.common.UnauthenticatedException;
 import co.cask.cdap.proto.ConfigEntry;
+import co.cask.cdap.proto.DatasetMeta;
 import co.cask.cdap.proto.ProgramRunStatus;
+import co.cask.cdap.proto.id.DatasetId;
+import co.cask.cdap.proto.id.StreamId;
 import co.cask.cdap.security.spi.authorization.UnauthorizedException;
 import co.cask.cdap.test.ApplicationManager;
 import co.cask.cdap.test.LongRunningTestBase;
@@ -36,10 +39,17 @@ import com.google.common.collect.Sets;
 import com.google.common.io.ByteStreams;
 import com.google.common.reflect.TypeToken;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellScanner;
 import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.client.Table;
 import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -47,10 +57,10 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -88,8 +98,8 @@ public class InvalidListPruneTest extends LongRunningTestBase<InvalidListPruneTe
     // Wait for the previous mapreduce job to finish running
     ApplicationManager applicationManager = getApplicationManager();
     MapReduceManager mrManager = applicationManager.getMapReduceManager(InvalidTxGeneratorApp.InvalidMapReduce.MR_NAME);
+    // TODO: Would be good to get only required number of history entries
     mrManager.waitForRuns(ProgramRunStatus.FAILED, iteration, 5, TimeUnit.MINUTES);
-
   }
 
   @Override
@@ -97,7 +107,7 @@ public class InvalidListPruneTest extends LongRunningTestBase<InvalidListPruneTe
     // Verify that the invalid ids from the 5th iteration earlier have been pruned
     // TODO: This check has to be enhanced to take into account test iteration interval, prune interval, tx max lifetime
     int checkIteration = state.getIteration() - 5;
-    Set<Long> removeIds = new HashSet<>();
+    Set<Long> removeIds = new TreeSet<>();
     Map<Integer, List<Long>> newIterationState = new HashMap<>();
     for (Map.Entry<Integer, List<Long>> entry : state.getInvalidTxIds().entrySet()) {
       int iteration = entry.getKey();
@@ -111,6 +121,9 @@ public class InvalidListPruneTest extends LongRunningTestBase<InvalidListPruneTe
     Set<Long> currentInvalids = Sets.newHashSet(getInvalidList());
     Sets.SetView<Long> notRemovedIds = Sets.intersection(currentInvalids, removeIds);
     Assert.assertTrue("Expected the following invalid ids to be pruned: " + notRemovedIds, notRemovedIds.isEmpty());
+    LOG.info("Pruned invalid ids: {}", removeIds);
+
+    verifyInvalidDataRemoval(getLongRunningNamespace().dataset(InvalidTxGeneratorApp.DATASET), removeIds);
 
     return new InvalidListPruneTestState(state.getIteration(), newIterationState);
   }
@@ -122,7 +135,7 @@ public class InvalidListPruneTest extends LongRunningTestBase<InvalidListPruneTe
 
     flushAndCompactTables();
 
-    truncateAndSendEvents(InvalidTxGeneratorApp.STREAM, iteration);
+    truncateAndSendEvents(getLongRunningNamespace().stream(InvalidTxGeneratorApp.STREAM), iteration);
 
     // Now run the mapreduce job
     ApplicationManager applicationManager = getApplicationManager();
@@ -134,10 +147,10 @@ public class InvalidListPruneTest extends LongRunningTestBase<InvalidListPruneTe
     return new InvalidListPruneTestState(iteration, invalidTxIds);
   }
 
-  private void truncateAndSendEvents(String stream, int iteration)
+  private void truncateAndSendEvents(StreamId stream, int iteration)
     throws IOException, StreamNotFoundException, UnauthenticatedException, UnauthorizedException {
     StreamClient streamClient = getStreamClient();
-    streamClient.truncate(getLongRunningNamespace().stream(stream));
+    streamClient.truncate(stream);
 
     // Create unique events for this iteration using the iteration id as part of the event
     StringWriter writer = new StringWriter();
@@ -150,7 +163,7 @@ public class InvalidListPruneTest extends LongRunningTestBase<InvalidListPruneTe
     writer.write(InvalidTxGeneratorApp.EXCEPTION_STRING);
     writer.write("\n");
     LOG.info("Writing {} events in one batch to stream {}", BATCH_SIZE + 1, stream);
-    streamClient.sendBatch(getLongRunningNamespace().stream(stream), "text/plain",
+    streamClient.sendBatch(stream, "text/plain",
                            ByteStreams.newInputStreamSupplier(writer.toString().getBytes(Charsets.UTF_8)));
   }
 
@@ -159,17 +172,16 @@ public class InvalidListPruneTest extends LongRunningTestBase<InvalidListPruneTe
   }
 
   private void flushAndCompactTables() throws IOException, UnauthorizedException, UnauthenticatedException {
-    Connection connection = ConnectionFactory.createConnection(getHBaseConf());
-    LOG.info("Using connection: {}", connection.getConfiguration().get("hbase.zookeeper.quorum"));
-    HBaseAdmin admin = new HBaseAdmin(connection);
-    HTableDescriptor[] descriptors = admin.listTables();
-    for (HTableDescriptor descriptor : descriptors) {
-      LOG.info("Flushing table {}", descriptor.getTableName());
-      admin.flush(descriptor.getTableName());
-//        admin.compact(descriptor.getTableName());
-      LOG.info("Major compacting table {}", descriptor.getTableName());
-      admin.majorCompact(descriptor.getTableName());
-//        admin.flush(descriptor.getTableName());
+    try (Connection connection = ConnectionFactory.createConnection(getHBaseConf())) {
+      LOG.info("Using connection: {}", connection.getConfiguration().get("hbase.zookeeper.quorum"));
+      HBaseAdmin admin = new HBaseAdmin(connection);
+      HTableDescriptor[] descriptors = admin.listTables();
+      for (HTableDescriptor descriptor : descriptors) {
+        LOG.info("Flushing table {}", descriptor.getTableName());
+        admin.flush(descriptor.getTableName());
+        LOG.info("Major compacting table {}", descriptor.getTableName());
+        admin.majorCompact(descriptor.getTableName());
+      }
     }
   }
 
@@ -189,5 +201,52 @@ public class InvalidListPruneTest extends LongRunningTestBase<InvalidListPruneTe
     HttpResponse response = restClient.execute(HttpMethod.GET, config.resolveURL("transactions/invalid"),
                                                config.getAccessToken());
     return ObjectResponse.fromJsonBody(response, new TypeToken<List<Long>>() { }).getResponseObject();
+  }
+
+
+  private void verifyInvalidDataRemoval(DatasetId dataset, Set<Long> prunedTxIds)
+    throws IOException, UnauthorizedException, UnauthenticatedException {
+    if (prunedTxIds.isEmpty()) {
+      LOG.info("No pruned tx ids, not verifying data removal");
+      return;
+    }
+
+    RESTClient restClient = getRestClient();
+    ClientConfig config = getClientConfig();
+    HttpResponse response =
+      restClient.execute(HttpMethod.GET,
+                         config.resolveNamespacedURLV3(dataset.getParent(), "data/datasets/" + dataset.getDataset()),
+                         config.getAccessToken());
+    DatasetMeta datasetMeta =
+      ObjectResponse.fromJsonBody(response, new TypeToken<DatasetMeta>() { }).getResponseObject();
+    String datasetType = datasetMeta.getSpec().getType();
+    // We only know how to verify key value table!
+    // TODO: would be good to have the logic to get underlying HBase tables of any dataset
+    Assert.assertEquals("co.cask.cdap.api.dataset.lib.KeyValueTable", datasetType);
+    // KeyValueTable only has one underlying HBase table
+    String hbaseTableName = "cdap_" + dataset.getParent().getEntityName() + ":" +
+      datasetMeta.getSpec().getSpecifications().values().iterator().next().getName();
+
+    // Scan and make sure that invalid data is not present in the table
+    try (Connection connection = ConnectionFactory.createConnection(getHBaseConf())) {
+      LOG.info("Using connection: {} to scan {}",
+               connection.getConfiguration().get("hbase.zookeeper.quorum"), hbaseTableName);
+      Table table = connection.getTable(TableName.valueOf(hbaseTableName));
+      int cellCount = 0;
+      try (ResultScanner scanner = table.getScanner(new Scan())) {
+        Result next;
+        while ((next = scanner.next()) != null) {
+          CellScanner cellScanner = next.cellScanner();
+          while (cellScanner.advance()) {
+            Cell cell = cellScanner.current();
+            ++cellCount;
+            LOG.trace("Checking cell {}", cell);
+            Assert.assertFalse("Invalid data for pruned tx " + cell.getTimestamp() + " is still present ",
+                               prunedTxIds.contains(cell.getTimestamp()));
+          }
+        }
+      }
+      LOG.info("Verified that {} cells in dataset {} do not contain data from pruned transactions", cellCount, dataset);
+    }
   }
 }
