0e64ac51fb11eb52a5f44c33a5d1de627bb7738c
Chaoran Wang <superchaoran@gmail.com>
2017-05-30 19:01:27 -0700

    Adding group/role based stream version of SEC-AUTH-012

--- integration-test-remote/src/test/java/co/cask/cdap/security/StreamSecurityRoleGroupTest.java
+++ integration-test-remote/src/test/java/co/cask/cdap/security/StreamSecurityRoleGroupTest.java
@@ -196,6 +196,118 @@ public class StreamSecurityRoleGroupTest extends AudiTestBase {
     Assert.assertFalse(getNamespaceClient().exists(namespaceId));
   }
 
+  /**
+   * SEC-AUTH-019(STREAM) and Group and Role's version of SEC-AUTH-012
+   * Grant a user READ access on a stream. Try to get the stream from a program and call a READ method on it.
+   *
+   * There are three users in the system.
+   * Alice and Bob belong to group 'nscreator' and Eve doesn't belong to 'nscreator'.
+   * Now we assign a role which has write privileges to 'nscreator' group.
+   * Expected behavior would be that Alice and Bob can successfully write to stream,
+   * while Eve cannot.
+   *
+   * @throws Exception
+   */
+  @Test
+  public void SEC_AUTH_012() throws Exception {
+
+    //creating an adminClient
+    ClientConfig adminConfig = getClientConfig(fetchAccessToken(ADMIN_USER, ADMIN_USER));
+    RESTClient adminClient = new RESTClient(adminConfig);
+    adminClient.addListener(createRestClientListener());
+
+    //creating namespace with random name
+    String name = generateRandomName();
+    NamespaceMeta meta = new NamespaceMeta.Builder().setName(name).build();
+    getTestManager(adminConfig, adminClient).createNamespace(meta);
+
+    //start of client code here:
+    StreamClient streamAdminClient = new StreamClient(adminConfig, adminClient);
+    NamespaceId namespaceId = new NamespaceId(name);
+    //creating stream within the namespace created
+    StreamId streamId = namespaceId.stream("streamTest");
+    //creating a stream using admin client
+    streamAdminClient.create(streamId);
+    StreamProperties config = streamAdminClient.getConfig(streamId);
+    Assert.assertNotNull(config);
+
+    //now authorize WRITE access to role_write
+    AuthorizationClient authorizationClient = new AuthorizationClient(adminConfig, adminClient);
+    //Create write role, grant write
+
+    Role role_read = new Role(ROLE_READ);
+    try {
+      authorizationClient.createRole(role_read);
+    }catch(RoleAlreadyExistsException ex){
+      //user_role already exists, it's fine to move on from here
+    }
+    authorizationClient.grant(namespaceId, role_read, Collections.singleton(Action.READ));
+
+    //create a principal group nscreator which already exist in UNIX system and add role_write to the group
+    authorizationClient.addRoleToPrincipal(role_read, new Principal(NSCREATOR, GROUP));
+
+    //1. using the user Alice to read message on the stream, should succeed
+
+    streamAdminClient.sendEvent(streamId, " a b ");
+
+    //create user Alice
+    ClientConfig aliceConfig = getClientConfig(fetchAccessToken(ALICE, ALICE + PASSWORD_SUFFIX));
+    RESTClient aliceClient = new RESTClient(aliceConfig);
+    aliceClient.addListener(createRestClientListener());
+    //create alice client
+    StreamClient streamAliceClient = new StreamClient(aliceConfig, aliceClient);
+    //calling a read method from admin client should generate expected result, since alice successfully write to the stream and admin can retrieve it
+    List<StreamEvent> events = streamAliceClient.getEvents(streamId, 0, Long.MAX_VALUE, Integer.MAX_VALUE,
+                                                           Lists.<StreamEvent>newArrayList());
+
+    //Asserting what Carol read from stream matches what Admin put inside stream.
+    Assert.assertEquals(1, events.size());
+    Assert.assertEquals(" a b ", Bytes.toString(events.get(0).getBody()));
+
+
+    //2. using the user Bob to read message on the stream, should succeed
+
+    streamAdminClient.sendEvent(streamId, " c d ");
+
+    //create user Bob
+    ClientConfig bobConfig = getClientConfig(fetchAccessToken(BOB, BOB + PASSWORD_SUFFIX));
+    RESTClient bobClient = new RESTClient(aliceConfig);
+    bobClient.addListener(createRestClientListener());
+    //create bob client
+    StreamClient streamBobClient = new StreamClient(bobConfig, bobClient);
+
+    //calling a read method from admin client should generate expected result, since carol successfully write to the stream and admin can retrieve it
+    events = streamBobClient.getEvents(streamId, 0, Long.MAX_VALUE, Integer.MAX_VALUE,
+                                         Lists.<StreamEvent>newArrayList());
+
+    //Asserting what Carol read from stream matches what Admin put inside stream.
+    Assert.assertEquals(2, events.size());
+    Assert.assertEquals(" c d ", Bytes.toString(events.get(1).getBody()));
+
+    //3. using the user eve to write message on the stream, should fail
+    streamAdminClient.sendEvent(streamId, " e f ");
+    //create user Eve
+    ClientConfig eveConfig = getClientConfig(fetchAccessToken(EVE, EVE + PASSWORD_SUFFIX));
+    RESTClient eveClient = new RESTClient(eveConfig);
+    eveClient.addListener(createRestClientListener());
+    //create Eve client
+    StreamClient streamEveClient = new StreamClient(eveConfig, eveClient);
+
+
+    try {
+      //fail if Eve is allowed to read the stream here
+      streamEveClient.getEvents(streamId, 0, Long.MAX_VALUE, Integer.MAX_VALUE,
+                                Lists.<StreamEvent>newArrayList());
+      Assert.fail();
+    }catch(IOException ex){
+      //expected IOException 403 forbidden URL access here
+    }
+
+    // Now delete the namespace and make sure that it is deleted
+    getNamespaceClient().delete(namespaceId);
+    Assert.assertFalse(getNamespaceClient().exists(namespaceId));
+  }
+
   // We have to use RestClient directly to attempt to create a stream with an invalid name (negative test against the
   // StreamHandler), because the StreamClient throws an IllegalArgumentException when passing in an invalid stream name.
   private void createStream(String streamName)
