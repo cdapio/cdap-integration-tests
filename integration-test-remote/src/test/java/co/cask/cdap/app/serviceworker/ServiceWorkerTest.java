b190ada8c10d6e4d2c82a19c7bbdabef4a2787ad
Ali Anwar <anwar1@berkeley.edu>
2017-09-11 13:30:31 -0700

    Update package of ServiceWorkerTest, so that it doesn't run against the in-memory Standalone test runner, since it uses artifacts.

--- integration-test-remote/src/test/java/co/cask/cdap/app/serviceworker/ServiceWorkerTest.java
+++ integration-test-remote/src/test/java/co/cask/cdap/app/serviceworker/ServiceWorkerTest.java
@@ -14,7 +14,7 @@
  * the License.
  */
 
-package co.cask.cdap.apps.serviceworker;
+package co.cask.cdap.app.serviceworker;
 
 import co.cask.cdap.api.artifact.ArtifactInfo;
 import co.cask.cdap.api.artifact.ArtifactRange;
