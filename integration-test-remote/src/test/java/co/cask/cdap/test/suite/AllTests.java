90187b6dd432ba47e2d9f91878a536cb5225e4bd
Ali <alianwar@google.com>
2019-01-10 17:23:54 -0800

    CDAP-14746 Remove DatasetTest.

--- integration-test-remote/src/test/java/co/cask/cdap/test/suite/AllTests.java
+++ integration-test-remote/src/test/java/co/cask/cdap/test/suite/AllTests.java
@@ -38,7 +38,6 @@ import co.cask.cdap.app.restart.HangingWorkerTest;
 import co.cask.cdap.app.serviceworker.ServiceWorkerTest;
 import co.cask.cdap.apps.ApplicationTest;
 import co.cask.cdap.apps.ApplicationVersionTest;
-import co.cask.cdap.apps.DatasetTest;
 import co.cask.cdap.apps.NamespaceTest;
 import co.cask.cdap.apps.NamespacedStreamTest;
 import co.cask.cdap.apps.explore.ExploreTest;
@@ -66,7 +65,6 @@ import org.junit.runners.Suite;
   BatchAggregatorTest.class,
   BatchCubeSinkTest.class,
   BatchJoinerTest.class,
-  DatasetTest.class,
   DataStreamsTest.class,
   ETLMapReduceTest.class,
   ExcelInputReaderTest.class,
